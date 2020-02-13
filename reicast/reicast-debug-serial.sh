ffc8c005eabe8ea6b8d96e41d905677b3d88706e
cpasjuste <cpasjuste@gmail.com>
2020-02-13 21:55:19 +0100

    - use dreamboot "special" bios for reicast debug

--- reicast/reicast-debug-serial.sh
+++ reicast/reicast-debug-serial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f /tmp/ttyReicast
-reicast -config config:Debug.VirtualSerialPortFile=/tmp/ttyReicast -config config:Debug.VirtualSerialPort=yes "$1" &
-sleep 5
+reicast -config config:Debug.VirtualSerialPortFile=/tmp/ttyReicast -config config:Debug.VirtualSerialPort=yes bios &
+sleep 10
 dc-tool -t /tmp/ttyReicast -x "$2"
