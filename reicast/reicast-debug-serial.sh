ee8f9a46cd81c7c15346b93cd70f81dc9025233e
cpasjuste <cpasjuste@gmail.com>
2021-02-08 13:25:22 +0100

    cmake: remove reicast target (use cdi target)

--- reicast/reicast-debug-serial.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f /tmp/ttyReicast
-reicast -config config:Debug.VirtualSerialPortFile=/tmp/ttyReicast -config config:Debug.VirtualSerialPort=yes nodisk &
-sleep 10
-dc-tool -t /tmp/ttyReicast -x "$2"
